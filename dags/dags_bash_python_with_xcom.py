bb98c30ced177f5ca8e602c1e12620b15d58eebf
Leejungmin <jungmin127@gmail.com>
2024-07-04 23:19:51 +0900

    dags_bash_python_xcom

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -4,7 +4,7 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id='dags_bash_with_macro_eg1',
+    dag_id='dags_bash_python_with_xcom',
     schedule='30 9 * * *', 
     start_date=pendulum.datetime(2024, 6, 1, tz='Asia/Seoul'),
     catchup=False
