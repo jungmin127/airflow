b52b6c1a351ec2052b14e5667767826dc4fb9d65
Leejungmin <jungmin127@gmail.com>
2024-08-05 22:11:53 +0900

    args

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -6,7 +6,7 @@ from common.common_func import regist2
 with DAG(
     dag_id='dags_python_with_op_kwargs',
     schedule='30 6 * * *',
-    start_date=pendulum.datetime(2023, 3, 1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2024, 8, 5, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     
