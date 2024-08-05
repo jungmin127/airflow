8c6a2bb29f39955d8ac200a60ad9517668dfc0a9
Leejungmin <jungmin127@gmail.com>
2024-08-05 21:59:39 +0900

    task decorator

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -5,7 +5,7 @@ from airflow.decorators import task
 with DAG(
     dag_id="dag_python_task_decorator",
     schedule="0 2 * * 1",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 8, 5, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
