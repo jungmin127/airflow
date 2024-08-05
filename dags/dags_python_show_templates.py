3ab38e07fa6bd35ecb99ef90cba5e1a55f6eaed9
Leejungmin <jungmin127@gmail.com>
2024-08-05 22:35:28 +0900

    time

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -6,9 +6,9 @@ from airflow.decorators import task
 with DAG(
     dag_id='dags_python_show_templates',
     schedule='30 9 * * *',
-    start_date=pendulum.datetime(2024, 6, 15, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2024, 8, 1, tz='Asia/Seoul'),
     catchup=True  
-    #이 코드 작성일: 2024/7/1, catchup True 로 주고 15개 누적출력
+    #이 코드 작성일: 2024/8/5, catchup True 로 주고 4개 누적출력
 ) as dag:
     
     @task(task_id='python_task')
