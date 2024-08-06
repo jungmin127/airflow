8fcfd34475b834b9bdb6821138c76ea77103e4f9
Leejungmin <jungmin127@gmail.com>
2024-08-06 22:37:53 +0900

    bok test 1

---
+++ dags/dags_bok_1.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.decorators import task
+import pendulum
+
+with DAG(
+    dag_id='dags_bok_1',
+    start_date=pendulum.datetime(2024,8,6, tz='Asia/Seoul'),
+    catchup=False,
+    schedule=None
+) as dag:
+    
+    '''한국은행경제통계시스템'''
+    bok_kospi_info = SimpleHttpOperator(
+        task_id='bok_test_1',
+        http_conn_id='ecos.bok.or.kr/api/StatisticSearch/',
+        endpoint='{{var.value.apikey_openapi_bok}}/json/kr/1/2000/802Y001/D/20240101/20240731/0001000',
+        method='GET',
+        headers={'Content-Type': 'application/json',
+                        'charset': 'utf-8',
+                        'Accept': '*/*'
+                        }
+    )
+
+    @task(task_id='python_2')
+    def python_2(**kwargs):
+        ti = kwargs['ti']
+        rslt = ti.xcom_pull(task_ids='bok_test_1')
+        import json
+        from pprint import pprint
+
+        pprint(json.loads(rslt))
+
+    bok_kospi_info >> python_2()
