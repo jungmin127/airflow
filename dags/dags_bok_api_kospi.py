475366ada6bfc5befc0cc2e0cf6c01f3ab5cb8bf
Leejungmin <jungmin127@gmail.com>
2024-08-08 23:38:34 +0900

    bok test

--- dags/dags_bok_api_kospi.py
+++ dags/dags_bok_api_kospi.py
@@ -11,7 +11,7 @@ with DAG(
     '''일별 Kospi값'''
     kospi_value = BokKospiToDataFrameOperator(
         task_id='kospi_value',
-        path='/opt/airflow/files/kospi_value/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        path='/opt/airflow/files/kospi_value/{{ ds_nodash }}',
         file_name='kospi.csv'
     )
 
