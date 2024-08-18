57c2b7670a07ec00cf8fb67ff3bd835ada82aa85
Leejungmin <jungmin127@gmail.com>
2024-08-18 20:53:54 +0900

    'test'

---
+++ dags/dags_btc_ml.py
@@ -0,0 +1,55 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from common.hooks.custom_btc_postgres_hook import CustomBtcPostgresHook
+
+def query_data_from_postgres(postgres_conn_id, query, **kwargs):
+    hook = CustomBtcPostgresHook(postgres_conn_id=postgres_conn_id)
+    conn = hook.get_conn()
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
+
+def ml_model(**kwargs):
+    ti = kwargs['ti']
+    postgres_conn_id = kwargs['postgres_conn_id']
+
+    query = "SELECT candle_date_time_kst, opening_price, high_price, low_price, trade_price, candle_acc_trade_price\
+            FROM btc \
+            GROUP BY 1,2,3,4,5,6"
+    df = query_data_from_postgres(postgres_conn_id, query)
+
+    df['candle_date_time_kst'] = pd.to_datetime(df['candle_date_time_kst'], format = '%Y-%m-%d T%H:%M:%S')
+    df['year'] = df['candle_date_time_kst'].dt.year
+    df['month'] = df['candle_date_time_kst'].dt.month
+    df['day'] = df['candle_date_time_kst'].dt.day
+    df['hour'] = df['candle_date_time_kst'].dt.hour
+
+    X = df.drop(columns=['trade_price','candle_date_time_kst'])
+    y = df['trade_price']
+
+    X_tr, X_val, y_tr, y_val = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+    model = RandomForestRegressor(n_estimators=100, random_state=1234)
+    model.fit(X_tr, y_tr)
+
+    pred = model.predict(X_val)
+    score = mean_squared_error(y_val, pred)
+    print(f'Model Score: {mean_squared_error:.2f}')
+
+with DAG(
+    dag_id='dags_ml_model_training',
+    start_date=pendulum.datetime(2024, 8, 18, tz='Asia/Seoul'),
+    schedule='3 * * * *',
+    catchup=False
+) as dag:
+
+    btc_ml_model_task = PythonOperator(
+        task_id='btc_ml_model_task',
+        python_callable=ml_model,
+        op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom'}
+    )
