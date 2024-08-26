5b92daefb8aefae2b2f05a41d6ad8c2ff29d28fa
Leejungmin <jungmin127@gmail.com>
2024-08-26 23:59:42 +0900

    'test'

--- plugins/common/operators/duplicated_check.py
+++ plugins/common/operators/duplicated_check.py
@@ -6,30 +6,28 @@ from sqlalchemy import create_engine
 from airflow.models.baseoperator import BaseOperator
 
 class CompareDataOperator(BaseOperator):
-    def __init__(self, postgres_conn_id, **kwargs):
+    def __init__(self, path, file_name, table_name, postgres_conn_id, **kwargs):
+        super().__init__(**kwargs)  # Ensure proper initialization of the parent class
+        self.path = path
+        self.file_name = file_name
+        self.table_name = table_name
         self.postgres_conn_id = postgres_conn_id
-    
+
     def get_conn(self):
-        airflow_conn = BaseHook.get_connection(self.postgres_conn_id) 
+        airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
         self.host = airflow_conn.host
         self.user = airflow_conn.login
         self.password = airflow_conn.password
         self.dbname = airflow_conn.schema
         self.port = airflow_conn.port
 
-        self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
-        return self.postgres_conn
+        return psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
 
     def execute(self, context):
+        self.log.info("Executing CompareDataOperator...")
+
         # PostgreSQL connection setup
-        airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
-        conn = psycopg2.connect(
-            host=airflow_conn.host,
-            user=airflow_conn.login,
-            password=airflow_conn.password,
-            dbname=airflow_conn.schema,
-            port=airflow_conn.port
-        )
+        conn = self.get_conn()
         
         # Read file data
         file_path = os.path.join(self.path, self.file_name)
