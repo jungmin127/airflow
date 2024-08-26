ab372bbdd59a777112f1baad77b8aa273a673d4c
Leejungmin <jungmin127@gmail.com>
2024-08-26 23:53:42 +0900

    'test'

--- plugins/common/operators/duplicated_check.py
+++ plugins/common/operators/duplicated_check.py
@@ -6,12 +6,19 @@ from sqlalchemy import create_engine
 from airflow.models.baseoperator import BaseOperator
 
 class CompareDataOperator(BaseOperator):
-    def __init__(self, path, file_name, table_name, postgres_conn_id, **kwargs):
-        super().__init__(**kwargs)
-        self.path = path
-        self.file_name = file_name
-        self.table_name = table_name
+    def __init__(self, postgres_conn_id, **kwargs):
         self.postgres_conn_id = postgres_conn_id
+    
+    def get_conn(self):
+        airflow_conn = BaseHook.get_connection(self.postgres_conn_id) 
+        self.host = airflow_conn.host
+        self.user = airflow_conn.login
+        self.password = airflow_conn.password
+        self.dbname = airflow_conn.schema
+        self.port = airflow_conn.port
+
+        self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
+        return self.postgres_conn
 
     def execute(self, context):
         # PostgreSQL connection setup
