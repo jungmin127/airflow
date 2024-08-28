244cef387f6437c0a1bd401d8fa3cb32a23cc1a3
Leejungmin <jungmin127@gmail.com>
2024-08-28 21:53:56 +0900

    'test'

--- plugins/common/operators/fetch_crypto_data.py
+++ plugins/common/operators/fetch_crypto_data.py
@@ -46,6 +46,8 @@ class FetchLatestTradePriceOperator(BaseOperator):
         iloc_ranges = [24, 36, 48, 60, 120]
         rolling_windows = [5, 10, 15]
         filter_buy_df = pd.DataFrame(columns=['crypto', '(n)ma', 'datetime', 'close', 'ma'])
+        filter_sell_df = pd.DataFrame(columns=['crypto', '(n)ma', 'datetime', 'close', 'ma'])
+        filter_return_df = pd.DataFrame(columns=['crypto', '(n)ma', 'day', 'date', 'return(%)'])
 
         cryptos = df['market'].unique()
         for crypto in cryptos:
@@ -85,7 +87,101 @@ class FetchLatestTradePriceOperator(BaseOperator):
                             })
                             filter_buy_df = pd.concat([filter_buy_df, buy_row], ignore_index=True)
                             #filter_buy_df = filter_buy_df.drop_duplicates(['crypto', '(n)ma'], keep='last').reset_index(drop=True)
+
+        for crypto in cryptos:
+            crypto_df = df[df['market'] == crypto].copy()
+            for iloc_range in iloc_ranges:
+                for rolling_window in rolling_windows:
+                    temp_df = crypto_df[['candle_date_time_kst','trade_price']].iloc[iloc_range:].copy()
+                    temp_df['MA'] = temp_df['trade_price'].rolling(rolling_window).mean().shift(1)
+                    temp_df['ACTION'] = np.where(temp_df['trade_price'] > temp_df['MA'], 'buy', 'sell')
+
+                    cond_sell = (temp_df['ACTION'] == 'sell') & (temp_df['ACTION'].shift(1) == 'buy')
+                    df_sell = temp_df[cond_sell].reset_index(drop=True)
+                    df_sell.columns = ['candle_date_time_kst','trade_price', 'MA', 'ACTION']
+                    #df_sell['datetime'] = crypto_df['candle_date_time_kst'].iloc[cond_buy].values
+
+                    if not df_sell.empty:
+                        last_date_sell = df_sell.tail(1)['candle_date_time_kst'].values[0]
+                        last_date_sell = pd.to_datetime(last_date_sell)
+
+                        kst = pytz.timezone('Asia/Seoul')
+                        last_date_sell_kst = last_date_sell.tz_localize(kst, ambiguous='NaT')
+                        utc_time = datetime.now(timezone.utc)
+                        kst_time = utc_time.astimezone(kst)
+
+                        def get_previous_hour(dt):
+                            return dt.replace(minute=0, second=0, microsecond=0)
+
+                        previous_hour_time = get_previous_hour(kst_time)
+
+                        if last_date_sell_kst == previous_hour_time:
+                            sell_row = pd.DataFrame({
+                                'crypto': [crypto],
+                                '(n)ma': [rolling_window],
+                                'datetime': [last_date_sell],
+                                'close': [df_sell.tail(1)['trade_price'].values[0]],
+                                'ma': [temp_df['MA'].iloc[-1]]
+                            })
+                            filter_sell_df = pd.concat([filter_sell_df, sell_row], ignore_index=True)
+                            #filter_sell_df = filter_sell_df.drop_duplicates(['crypto', '(n)ma'], keep='last').reset_index(drop=True)          
+        
+        for crypto in cryptos:
+            crypto_df = df[df['market'] == crypto].copy()
+            for iloc_range in iloc_ranges:
+                for rolling_window in rolling_windows:
+                    temp_df = crypto_df[['candle_date_time_kst','trade_price']].iloc[iloc_range:].copy()
+                    temp_df['MA'] = temp_df['trade_price'].rolling(rolling_window).mean().shift(1)
+                    temp_df['ACTION'] = np.where(temp_df['trade_price'] > temp_df['MA'], 'buy', 'sell')
+
+                    cond_buy = (df['action'] == 'buy') & (df['action'].shift(1) == 'sell')
+                    cond_sell = (df['action'] == 'sell') & (df['action'].shift(1) == 'buy')
+                    temp_df.iloc[-1, -1] = 'sell'
+
+                    df_buy = df[cond_buy].reset_index()
+                    df_buy.columns = ['date(buy)', 'close(buy)', 'ma', 'action(buy)']
+                    df_sell = df[cond_sell].reset_index()
+                    df_sell.columns = ['date(sell)', 'close(sell)', 'ma', 'action(sell)']
+
+                    df_result = pd.concat([df_buy, df_sell], axis=1)
+                    df_result['return_rate'] = df_result['close(sell)'] / df_result['close(buy)']
+                    df_result = df_result.dropna()
+
+                    kst = pytz.timezone('Asia/Seoul')
+                    last_date_sell_kst = last_date_sell.tz_localize(kst, ambiguous='NaT')
+                    utc_time = datetime.now(timezone.utc)
+                    kst_time = utc_time.astimezone(kst)
+
+                    def get_previous_hour(dt):
+                            return dt.replace(minute=0, second=0, microsecond=0)
+
+                    previous_hour_time = get_previous_hour(kst_time)
+
+                    if not df_result.empty:
+                        final_return = round((df_result[['return_rate']].cumprod().iloc[-1, -1] - 1) * 100, 1)
+                        return_row = pd.DataFrame({
+                            'crypto': [crypto],
+                            '(n)ma': [rolling_window],
+                            'day': [iloc_range],
+                            'date': [previous_hour_time],
+                            'return(%)': [f"{final_return:.1f}"]
+                        })
+                        filter_return_df = pd.concat([filter_return_df, return_row], ignore_index=True)
+                        #filter_return_df = filter_return_df.astype({'(n)ma': int, 'day': int})
+                        #filter_return_df['return(%)'] = filter_return_df['return(%)'].apply(lambda x: round(float(x), 1)).astype(float)
+                        #filter_return_df['date'] = filter_return_df['date'].apply(lambda x: x.to_timestamp() if isinstance(x, pd.Period) else x)
+
         if not filter_buy_df.empty:
-            self.log.info(f"Filtered buy DataFrame:\n{filter_buy_df.to_string(index=False)}")
+            self.log.info(f"{filter_buy_df.to_string(index=False)}")
         else:
-            self.log.info("No buy conditions met. Filtered buy DataFrame is empty.")
+            self.log.info("조건을 만족하는 코인 없음") 
+
+        if not filter_sell_df.empty:
+            self.log.info(f"{filter_sell_df.to_string(index=False)}")
+        else:
+            self.log.info("조건을 만족하는 코인 없음") 
+
+        if not filter_return_df.empty:
+            self.log.info(f"{filter_return_df.to_string(index=False)}")
+        else:
+            self.log.info("코드에 문제가 있는 상태") 
